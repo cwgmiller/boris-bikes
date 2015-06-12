69f67da63d22aea4e457fffae9f32dfa75b8b111
Christian Miller <cwgmiller@gmail.com>
2015-06-12 10:30:57 +0100

    Resolving Spec Merge Conflicts

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,5 @@
 require 'bike'
 
-<<<<<<< HEAD
 describe Bike do
 
   it { is_expected.to respond_to :working? }
@@ -11,15 +10,3 @@ describe Bike do
   end
 
 end
-=======
-describe Bike do 
-	it {is_expected.to respond_to :working?}
-
-	it 'can be reported broken' do 
-		subject.report_broken
-		expect(subject).to be_broken
-	end
-
-	it {is_expected.to respond_to :broken?}
-end
->>>>>>> c3c184d7c85b316ac14b6ed69cbbc9a31b290227
